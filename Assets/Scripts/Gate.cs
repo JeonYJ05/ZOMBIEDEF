0bd4b862ed9ec684ab4cfd0f0ee3a1fe7f586d06
JeonYJ05 <136886537+JeonYJ05@users.noreply.github.com>
2024-01-29 23:51:05 +0900

    add GateFunction

---
+++ Assets/Scripts/Gate.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace YJ.Zombie.GameManage
+{
+    public class Gate : MonoBehaviour
+    {
+        public GameObject Ui;
+        private void OnTriggerEnter(Collider other)
+        {
+            if(!other.CompareTag("Player"))
+            {
+                Debug.Log("다음 단계");
+                Toggle();
+            }
+           
+        }
+        public void Toggle()
+        {
+            Ui.SetActive(!Ui.activeSelf);
+
+            if (Ui.activeSelf)
+            {
+                Time.timeScale = 0f;
+            }
+            else
+            {
+                Time.timeScale = 1f;
+            }
+
+        }
+    }
+}
